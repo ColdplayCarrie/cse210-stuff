df6c9c911d1807a860d42b0e9f4f2b7e2250ebe2
ColdplayCarrie <yearbookstuff5@gmail.com>
2024-11-09 16:58:27 -0600

    Change from Hello to: program for letter grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade percentage? ");
+        string userGradePercentage = Console.ReadLine();
+        int gradePercentageNumber = int.Parse(userGradePercentage);
+
+        string letter = "";
+
+        if (gradePercentageNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentageNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentageNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentageNumber >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+        
+            if (gradePercentageNumber >=70)
+            {
+                Console.Write("Congratulations! You passed!");
+            }
+            else
+            {
+                Console.Write("You didn't pass yet. Try again! You'll get it!");
+            }
+    }    
 }
