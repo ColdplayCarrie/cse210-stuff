3e6d0ba7b336dea15cc6a55817f8fb072f99a526
ColdplayCarrie <yearbookstuff5@gmail.com>
2024-11-09 20:30:43 -0600

    changed Hello to: ask name, fav number, and tell the square

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int square = SquareNumber();
+
+        Console.WriteLine($"{userName}, the square of your number is {square}.");
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int favNumber = int.Parse(Console.ReadLine());
+            return favNumber;
+        }
+
+        static int SquareNumber()
+        {
+            int favNumber = PromptUserNumber();
+            int square = favNumber * favNumber;
+            return square;
+        }
+
     }
 }
