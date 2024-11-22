d98292fb70f2bdb300f75c1f5c8431af10bee57e
ColdplayCarrie <yearbookstuff5@gmail.com>
2024-11-21 19:12:14 -0600

    changed hello to: a fraction/decimal program with constructors,getters,setters

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        //Console.WriteLine($"{fraction1._top}/{fraction1._bottom}");
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(6);
+        //Console.WriteLine($"{fraction2._top}/{fraction2._bottom}");
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(6, 7);
+        //Console.WriteLine($"{fraction3._top}/{fraction3._bottom}");
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
     }
 }
