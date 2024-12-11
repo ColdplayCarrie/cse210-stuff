7beb26e532d5332be27d105415b08e0a4b3ed67d
ColdplayCarrie <yearbookstuff5@gmail.com>
2024-12-10 19:00:54 -0600

    changed hello to a program that lists student name, topic, title, etc.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Bob Bobberton", "American Sign Language");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Rob Robberton", "British Sign Language", "Section 7.3", "Problems 10-23");
+        Console.WriteLine($"{mathAssignment1.GetSummary()}, {mathAssignment1.GetHomeworkList()}");
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Bab Babberton", "Computer Science Language", "Is C# as Difficult as I'm Making It?");
+        Console.WriteLine($"{writingAssignment1.GetSummary()}, {writingAssignment1.GetWritingInformation()}");
     }
 }
